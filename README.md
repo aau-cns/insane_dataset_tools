e679a1df75f7d0ef8ce36a9998f5b7fb14f83b1c
Christian Brommer <christian.brommer@aau.at>
2022-12-08 15:11:10 +0100

    add: csv to rosbag export script and readme

--- README.md
+++ README.md
@@ -1,15 +1,130 @@
-# The INSANE Dataset Tools are "coming online soon"
+![INSANE Logo](./resources/insane_logo.png)
 
-In the meantime, you can contact the authors for further information:
+# Introduction
 
-[Christian Brommer](mailto:christian.brommer@aau.at)
-
-# Description
+[![arXiv](https://img.shields.io/badge/arXiv-10.48550/ARXIV.2210.09114-b31b1b.svg)](https://arxiv.org/abs/2210.09114) [![License](https://img.shields.io/badge/License-AAUCNS-green.svg)](./LICENSE)
 
 The INSANE data set is a multi-sensor cross-domain UAV data set (18 sensors) with accurate and absolute 6 DoF ground truth. The scenarios include indoor flights in a controlled environment with motion capture ground truth, outdoor-to-indoor transition flights with continuous ground truth, and extensive coverage of Mars analog data with the same vehicle. Mars analog data includes segments with various ground structures, cliff flight over, and cliff-wall traversing trajectories for mapping.
 
 This data set is ideal for testing novel algorithms with real-world sensor data and corresponding effects such as sensor degradation. Dedicated raw data for customized sensor calibration routines and vibration data for vehicle integrity tests are provided.
 
-The preprint is available at https://arxiv.org/abs/2210.09114
+**Preprint** available at: https://arxiv.org/abs/2210.09114
 
-and the entire data is available on our webpage https://www.aau.at/intelligente-systemtechnologien/control-of-networked-systems/datasets/
+**Data set** available on our webpage: https://www.aau.at/intelligente-systemtechnologien/control-of-networked-systems/datasets/
+
+**Data set Features:**
+
+- 6 DoF absolute ground~truth with centimeter and sub-degree accuracy 1-sigma for outdoor data sets.
+- Indoor trajectories with motion capture ground truth (6 DoF millimeter and sub-degree accuracy) for the initial proof of algorithms.
+- Outdoor to indoor transition trajectories with continuous absolute ground truth.
+- Trajectories in a Mars analog desert environment for Mars-Helicopter analog setups, including various ground structures, cliff flight over, and cliff-wall traversing trajectories for mapping.
+- Vehicle and sensor integrity, including intrinsic information such as static IMU data and RPM-correlated vibration data.
+- Real-world sensor effects and degradation posed by individual scenarios.
+- Initialization sequences for VIO algorithms.
+- Inter-sensor calibrations in pre-calculated form and raw calibration data sequences for custom calibration routines.
+
+The data which you can download from our website is ready to use. However, if you want to customize individual components, then all necessary steps are described below.
+
+**In the following, we provide scripts and their description for:**
+
+- Sensor calibration
+- Data export of the original ROS bag files
+- Ground-truth generation and transition segment alignment
+- and sensor CSV data to ROS bag exports 
+
+## Dependencies
+
+The majority of the scripts are Matlab files and do not require external dependencies. A few post-processing scripts are written in Python and require additional packages and a dedicated catkin workspace for custom ROS messages.
+
+### System Dependencies
+
+Dependencies can be installed as follows:
+
+```sh
+apt install python3 python3-pip python3-catkin-tools ros-noetic-tf ros-noetic-cv-bridge ros-noetic-image-transport
+pip3 install -U rospkg numpy tqdm scipy py3rosmsgs pycryptodomex
+```
+
+If you want to use a docker container, please also install the following dependencies for CV2:
+
+```sh
+apt-get install ffmpeg libsm6 libxext6
+```
+
+### ROS Dependencies
+
+The data set uses two non-standard ROS messages `MotorSpeed.msg` and `TagDistance.msg`. Both message definitions are located in `catkin_ws/src/insane_msgs`. These messages have to be build for the export script and further usage.
+
+```sh
+$ cd catkin_ws
+$ catkin init
+$ catkin build fiducial_msgs insane_msgs
+$ source ./devel/setup.bash
+```
+
+#### Additional steps for non-Python 3 ROS installs (<Noetic)
+
+If you are using a ROS version below Noetic, then the following steps are required to build **cv bridge** module against Python3
+
+Create a dedicated catkin workspace:
+```sh
+mkdir -p ~/cvbridge_build_ws/src && cd ~/cvbridge_build_ws/src && git clone -b noetic https://github.com/ros-perception/vision_opencv.git
+```
+Adapt the following lines in the `CMakeLists.txt` of the `ros-perception/vision_opencv` package:
+
+```cmake
+# modification at Line 11, changing:
+find_package(Boost REQUIRED python37)
+# to
+find_package(Boost REQUIRED python-py35)
+```
+
+Compile against the Python 3 library:
+```sh
+$ catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.5m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.5m.so
+$ catkin build
+$source ~/cvbridge_build_ws/devel/setup.bash
+```
+
+# Export CSV and PNG to ROS Bag
+
+If you downloaded the exported CSV and PNG files from our website and would like to use the data as a rosbag file, then you can use the following python script `post_scripts/csv2bag.py`. This requires the installation of dependencies and custom ros messages as described [here](#dependencies).
+
+Run the script inside a folder which only contains the data for one dataset e.g., as shown below:
+
+```
+mars_4
+├── mars_4_sensors
+├── mars_4_nav_cam
+└── mars_4_stereo_cam
+```
+
+The script will scan the directory for the expected CSV files and generate a bagfile `insane_dataset.bag`.
+
+# Contact
+
+For further information, please contact [Christian Brommer](mailto:christian.brommer@aau.at)
+
+# License
+
+This software is made available to the public to use (_source-available_), licensed under the terms of the BSD-2-Clause-License with no commercial use allowed, the full terms of which are made available in the [LICENSE](./LICENSE) file. No license in patents is granted.
+
+## Usage for academic purposes
+
+If you use this software in an academic research setting, please cite the corresponding paper and consult the [LICENSE](./LICENSE) file for a detailed explanation.
+
+```latex
+@article{brommer_insane_2022,
+	title     = {{INSANE}: Cross-Domain {UAV} Data Sets with Increased Number of Sensors for developing
+	             Advanced and Novel Estimators},
+	author    = {Brommer, Christian and Fornasier, Alessandro and Scheiber, Martin and 
+	             Delaune, Jeff and Brockers, Roland and Steinbrener, Jan and Weiss, Stephan},
+	year      = 2022,
+	month     = oct,
+	publisher = {arXiv},
+	doi       = {10.48550/arXiv.2210.09114},
+	url       = {http://arxiv.org/abs/2210.09114},
+	note      = {arXiv:2210.09114 [cs]},
+	keywords  = {Computer Science - Robotics}
+}
+```
---
+++ post_scripts/csv2bag.py
@@ -0,0 +1,481 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 Christian Brommer, Control of Networked Systems, University of Klagenfurt, Austria.
+#
+# All rights reserved.
+#
+# This software is licensed under the terms of the BSD-2-Clause-License with
+# no commercial use allowed, the full terms of which are made available
+# in the LICENSE file. No license in patents is granted.
+#
+# You can contact the author at <christian.brommer@ieee.org>
+
+
+# The script reads all sensor files from one experiment and generates a rosbag file for it.
+# NOTE: All timestamps are offset by 1e-9 seconds because this is the minimal time for ROS headers
+
+import os
+import csv
+import rosbag
+import rospy
+
+from sensor_msgs.msg import Imu, NavSatFix, MagneticField, FluidPressure, Range, CameraInfo
+from geometry_msgs.msg import TwistWithCovarianceStamped, PoseStamped, PoseWithCovarianceStamped
+from insane_msgs.msg import MotorSpeed, TagDistance
+
+import sys
+
+# Sanitize python path to ensure the import of cv2 for python3
+# in the presence of ROS Python 2.7 paths
+ros_path = '/opt/ros/kinetic/lib/python2.7/dist-packages'
+if ros_path in sys.path:
+    sys.path.remove(ros_path)
+    import cv2 as cv
+
+    sys.path.append(ros_path)
+else:
+    import cv2 as cv
+
+from cv_bridge import CvBridge
+
+bagfile_name = "insane_dataset.bag"
+min_ros_hdr_time = 1e-9
+
+
+def write_imu_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Writing ' + sensor_name + ' data to Bag...')
+
+    imu = Imu()  # IMU message
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write IMU message
+            imu.header.seq = seq_count
+            imu.header.stamp = time
+            imu.linear_acceleration.x = float(row['a_x'])
+            imu.linear_acceleration.y = float(row['a_y'])
+            imu.linear_acceleration.z = float(row['a_z'])
+            imu.angular_velocity.x = float(row['w_x'])
+            imu.angular_velocity.y = float(row['w_y'])
+            imu.angular_velocity.z = float(row['w_z'])
+
+            # Record message into the bag file
+            bag.write(topic_name, imu, time)
+    print('...done')
+    return
+
+
+def write_mag_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    mag_msg = MagneticField()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Timed Position message
+            mag_msg.header.seq = seq_count
+            mag_msg.header.stamp = time
+            mag_msg.header.frame_id = 'world'  # World frame
+            mag_msg.magnetic_field.x = float(row['cart_x'])
+            mag_msg.magnetic_field.y = float(row['cart_y'])
+            mag_msg.magnetic_field.z = float(row['cart_z'])
+
+            # Record message into the bag file
+            bag.write(topic_name, mag_msg, time)
+    print('...done')
+    return
+
+
+def write_baro_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    baro_msg = FluidPressure()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write timed baro message
+            baro_msg.header.seq = seq_count
+            baro_msg.header.stamp = time
+            baro_msg.header.frame_id = 'world'  # World frame
+            baro_msg.fluid_pressure = float(row['p'])
+
+            # Record message into the bag file
+            bag.write(topic_name, baro_msg, time)
+    print('...done')
+    return
+
+
+def write_uwb_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    uwb_msg = TagDistance()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Timed Position message
+            uwb_msg.header.seq = seq_count
+            uwb_msg.header.stamp = time
+            uwb_msg.valid[0] = bool(int(row['valid_m1']))
+            uwb_msg.distance[0] = float(row['dist_m1'])
+            uwb_msg.valid[1] = bool(int(row['valid_m2']))
+            uwb_msg.distance[1] = float(row['dist_m2'])
+            uwb_msg.valid[2] = bool(int(row['valid_m3']))
+            uwb_msg.distance[2] = float(row['dist_m3'])
+            uwb_msg.valid[3] = False
+            uwb_msg.distance[3] = 0
+
+            # Record message into the bag file
+            bag.write(topic_name, uwb_msg, time)
+    print('...done')
+    return
+
+
+def write_rpm_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    rpm_msg = MotorSpeed()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Timed Position message
+            rpm_msg.header.seq = seq_count
+            rpm_msg.header.stamp = time
+            rpm_msg.rpm[0] = float(row['motor1'])
+            rpm_msg.rpm[1] = float(row['motor2'])
+            rpm_msg.rpm[2] = float(row['motor3'])
+            rpm_msg.rpm[3] = float(row['motor4'])
+
+            # Record message into the bag file
+            bag.write(topic_name, rpm_msg, time)
+    print('...done')
+    return
+
+
+def write_lrf_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    lrf_msg = Range()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Timed Position message
+            lrf_msg.header.seq = seq_count
+            lrf_msg.header.stamp = time
+            lrf_msg.header.frame_id = 'world'  # World frame
+            lrf_msg.radiation_type = Range().INFRARED
+
+            lrf_msg.range = float(row['range'])
+
+            # Record message into the bag file
+            bag.write(topic_name, lrf_msg, time)
+    print('...done')
+    return
+
+
+def write_gps_vel_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    gps_vel_msg = TwistWithCovarianceStamped()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Velocity message
+            gps_vel_msg.header.seq = seq_count
+            gps_vel_msg.header.stamp = time
+            gps_vel_msg.header.frame_id = 'world'  # World frame
+            gps_vel_msg.twist.twist.linear.x = float(row['v_x'])
+            gps_vel_msg.twist.twist.linear.y = float(row['v_y'])
+            gps_vel_msg.twist.twist.linear.z = float(row['v_z'])
+
+            # Record message into the bag file
+            bag.write(topic_name, gps_vel_msg, time)
+
+    print('...done')
+    return
+
+
+def write_gps_coord_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    gps_coord_msg = NavSatFix()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Coordinate message
+            gps_coord_msg.header.seq = seq_count
+            gps_coord_msg.header.stamp = time
+            gps_coord_msg.header.frame_id = 'world'  # World frame
+            gps_coord_msg.latitude = float(row['lat'])
+            gps_coord_msg.longitude = float(row['long'])
+            gps_coord_msg.altitude = float(row['alt'])
+
+            gps_coord_msg.position_covariance[0] = float(row['cov_p_x'])
+            gps_coord_msg.position_covariance[4] = float(row['cov_p_y'])
+            gps_coord_msg.position_covariance[5] = float(row['cov_p_z'])
+
+            # Record message into the bag file
+            bag.write(topic_name, gps_coord_msg, time)
+
+    print('...done')
+    return
+
+
+def write_gps_enu_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    gps_enu_msg = PoseWithCovarianceStamped()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Position message
+            gps_enu_msg.header.seq = seq_count
+            gps_enu_msg.header.stamp = time
+            gps_enu_msg.header.frame_id = 'world'  # World frame
+            gps_enu_msg.pose.pose.position.x = float(row['p_x'])
+            gps_enu_msg.pose.pose.position.y = float(row['p_y'])
+            gps_enu_msg.pose.pose.position.z = float(row['p_z'])
+
+            gps_enu_msg.pose.covariance[0] = float(row['cov_p_x'])
+            gps_enu_msg.pose.covariance[7] = float(row['cov_p_y'])
+            gps_enu_msg.pose.covariance[14] = float(row['cov_p_z'])
+
+            # Record message into the bag file
+            bag.write(topic_name, gps_enu_msg, time)
+
+    print('...done')
+    return
+
+
+def write_pose_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+
+    pose_msg = PoseStamped()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t']) + min_ros_hdr_time)
+
+            # Write Position message
+            pose_msg.header.seq = seq_count
+            pose_msg.header.stamp = time
+            pose_msg.header.frame_id = 'world'  # World frame
+            pose_msg.pose.position.x = float(row['p_x'])
+            pose_msg.pose.position.y = float(row['p_y'])
+            pose_msg.pose.position.z = float(row['p_z'])
+
+            pose_msg.pose.orientation.w = float(row['q_w'])
+            pose_msg.pose.orientation.x = float(row['q_x'])
+            pose_msg.pose.orientation.y = float(row['q_y'])
+            pose_msg.pose.orientation.z = float(row['q_z'])
+
+            # Record message into the bag file
+            bag.write(topic_name, pose_msg, time)
+        print('...done')
+    return
+
+
+def write_img_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+    img_folder = "img"
+    cv_bridge = CvBridge()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t[ns]']) + min_ros_hdr_time)
+
+            root_path = os.path.join(os.path.split(csv_file)[0], img_folder)
+            if not os.path.exists(root_path):
+                print("Image file path " + root_path + "does not exist")
+                return
+
+            filename = str(row['filename']) + ".png"
+            image_file = os.path.join(root_path, filename)
+            cv_img = cv.imread(image_file, cv.IMREAD_GRAYSCALE)
+
+            # Write Timed Position message
+            image_msg = cv_bridge.cv2_to_imgmsg(cv_img, encoding="passthrough")
+            image_msg.header.seq = seq_count
+            image_msg.header.stamp = time
+            image_msg.header.frame_id = 'world'  # World frame
+
+            # Record message into the bag file
+            bag.write(topic_name+"/image_raw", image_msg, time)
+
+            # Add empty camera info msg
+            cam_info_msg = CameraInfo()
+            cam_info_msg.header.seq = seq
+            cam_info_msg.header.stamp = time
+
+            bag.write(topic_name+"/camera_info", cam_info_msg, time)
+    print('...done')
+    return
+
+
+def write_img_stereo_msg(csv_file, topic_name, bag, sensor_name, seq):
+    print('Reading ' + sensor_name + ' Data')
+    img_folder = "img"
+    cv_bridge = CvBridge()
+
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for seq_count, row in enumerate(reader, start=seq):
+            # Update time for message and wall time
+            time = rospy.Time.from_sec(float(row['t[ns]']) + min_ros_hdr_time)
+
+            root_path = os.path.join(os.path.split(csv_file)[0], img_folder)
+            if not os.path.exists(root_path):
+                print("Image file path " + root_path + "does not exist")
+                return
+
+            filename = str(row['filename']) + ".png"
+            image_file = os.path.join(root_path, filename)
+            cv_img = cv.imread(image_file, cv.IMREAD_GRAYSCALE)
+
+            # Write Timed Position message
+            image_msg = cv_bridge.cv2_to_imgmsg(cv_img, encoding="passthrough")
+            image_msg.header.seq = seq_count
+            image_msg.header.stamp = time
+            image_msg.header.frame_id = 'world'  # World frame
+
+            if filename.find("c1") != -1:
+                pre_topic_name = topic_name + "1"
+            else:
+                pre_topic_name = topic_name + "2"
+
+            # Record message into the bag file
+            bag.write(pre_topic_name+"/image_raw", image_msg, time)
+
+            # Add empty camera info msg
+            cam_info_msg = CameraInfo()
+            cam_info_msg.header.seq = seq
+            cam_info_msg.header.stamp = time
+
+            bag.write(pre_topic_name+"/camera_info", cam_info_msg, time)
+    print('...done')
+    return
+
+
+class data_module:
+    def __init__(self, sensor_name, filename, topic, export_fun):
+        self.name = sensor_name
+        self.found = False
+        self.filename = filename
+        self.topic = topic
+        self.export_fun = export_fun
+        self.path = ""
+
+    def __str__(self):
+        return "Name: " + self.name + " Topic: " + self.topic + " File: " + self.filename
+
+
+def main():
+    # Search for csv sensor files in current directory
+    dir_list = dict()
+    current_path = os.getcwd()
+    for root, dirs, files in os.walk(current_path):
+        for name in files:
+            if name.endswith(".csv"):
+                if 'name' in dir_list:
+                    print("Warning: " + name + " was found previously")
+                    continue
+
+                dir_list[name] = root
+
+    # Define sensor modules and associated csv files for export
+    sensor_list = []
+
+    sensor_list.append(data_module("PX4_IMU", "px4_imu.csv", "/mavros/imu/data_raw", write_imu_msg))
+    sensor_list.append(data_module("PX4_mag", "px4_mag.csv", "/mavros/imu/mag", write_mag_msg))
+    sensor_list.append(data_module("PX4_baro", "px4_baro.csv", "/mavros/imu/static_pressure", write_baro_msg))
+    sensor_list.append(data_module("PX4_GPS_coord", "px4_gps.csv", "/mavros/global_position/raw/fix", write_gps_coord_msg))
+    sensor_list.append(data_module("PX4_GPS_vel", "px4_gps_vel_data.csv", "/mavros/global_position/raw/gps_vel", write_gps_vel_msg))
+    sensor_list.append(data_module("PX4_RPM", "px4_rpm.csv", "/mavros/motor_speeds/speed", write_rpm_msg))
+    sensor_list.append(data_module("LSM_IMU", "lsm_imu.csv", "/imu_lsm9ds1/imu", write_imu_msg))
+    sensor_list.append(data_module("LSM_mag", "lsm_mag.csv", "/imu_lsm9ds1/mag", write_mag_msg))
+    sensor_list.append(data_module("UWB_range", "uwb_range.csv", "/uwb_trek/tagDistance_raw", write_uwb_msg))
+    sensor_list.append(data_module("RTK1_coord", "rtk_gps1.csv", "/rtk_gps_1/fix", write_gps_coord_msg))
+    sensor_list.append(data_module("RTK1_enu", "rtk_gps1.csv", "/rtk_gps_1/enu", write_gps_enu_msg))
+    sensor_list.append(data_module("RTK1_vel", "rtk_gps1.csv", "/rtk_gps_1/fix_velocity", write_gps_vel_msg))
+    sensor_list.append(data_module("RTK2_coord", "rtk_gps2.csv", "/rtk_gps_2/fix", write_gps_coord_msg))
+    sensor_list.append(data_module("RTK2_enu", "rtk_gps2.csv", "/rtk_gps_2/enu", write_gps_enu_msg))
+    sensor_list.append(data_module("RTK2_vel", "rtk_gps2.csv", "/rtk_gps_2/fix_velocity", write_gps_vel_msg))
+    sensor_list.append(data_module("MoCap", "mocap_vehicle_data.csv", "/vrpn_client/raw_transform", write_pose_msg))
+    sensor_list.append(data_module("RS_IMU", "rs_imu.csv", "/realsense/imu", write_imu_msg))
+    sensor_list.append(data_module("RS_odom", "rs_odom.csv", "/realsense/odom/sample", write_pose_msg))
+    sensor_list.append(data_module("LRF", "lrf_range.csv", "/lidar_lite/range", write_lrf_msg))
+    sensor_list.append(data_module("Nav_cam", "nav_cam_timestamps.csv", "/nav_cam", write_img_msg))
+    sensor_list.append(data_module("RS_stereo_cam", "stereo_cam_timestamps.csv", "/realsense/fisheye", write_img_stereo_msg))
+    sensor_list.append(data_module("ground_truth", "ground_truth_8hz.csv", "/gt", write_pose_msg))
+    sensor_list.append(data_module("ground_truth_interpolated", "ground_truth_80hz.csv", "/gt_inter", write_pose_msg))
+
+
+    # Check if files for the defined sensors exist and associate the file path
+    not_found_sensors = []
+    print("Found file for: ")
+    for sensor in sensor_list:
+        if sensor.filename in dir_list:
+            print('\t', end='')
+            print(sensor)
+            root_dir = dir_list[sensor.filename]
+            file_name = sensor.filename
+            sensor.path = os.path.join(root_dir, sensor.filename)
+            sensor.found = True
+        else:
+            not_found_sensors.append(sensor.filename)
+            sensor.found = False
+
+    # Print not found files
+    if len(not_found_sensors) > 0:
+        print("Did not find files for:")
+        for filename in not_found_sensors:
+            print("\t- " + filename)
+
+    # Open file for rosbag and write data for sensor modules whose data was present
+    with rosbag.Bag(os.path.join(current_path, bagfile_name), 'w') as bagfile:
+        # Write all sensors with associated files
+        for sensor in sensor_list:
+            if sensor.found:
+                sensor.export_fun(sensor.path, sensor.topic, bagfile, sensor.name, 1)
+
+if __name__ == "__main__":
+    main()
+
